9443d642c68a07e7c3a43d458d887612d9f5fc4b
R-YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY-N <Aba7sain@gmail.com>
2024-09-26 07:44:22 +0300

    ListTester circularLeftShift test1

--- src/main/java/com/example/project/ListTester.java
+++ src/main/java/com/example/project/ListTester.java
@@ -3,11 +3,21 @@ package com.example.project;
 public class ListTester {
     public static <T> void circularLeftShift(List <T> list, int n)
     {
-        throw new UnsupportedOperationException("Not supported yet.");
         // Write the method circularLeftShift, user of List ADT, that takes as input a non-empty
         // List list and an integer n > 0 and performs n circular left shift of the list.
         // Example 2.1. Given the list l : A, B, C, D, E, circularShiftLeft(l, 1) results in
         // B, C, D, E, A, circularShiftLeft(l, 2) results in C, D, E, A, B.
+    	if(!list.empty() && n > 0) {
+    		for(int i = 0; i < n; i++) {
+    			T temp = list.retrieve();
+    			do {
+    				list.findNext();
+    			} while (!list.last());
+    			list.insert(temp);
+    			list.findFirst();
+    			list.remove();
+    		}
+    	}
     }
     public static <T> void reverseCopy(DLL<T> l1, DLL<T> l2)
     {
